f877645c336d61deb3a1c007776cff8d184a2d28
shennan-wong-partior <shennan.wong@partior.com>
2023-07-20 17:46:14 +0000

    init

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
+  },
+  output: "export",
+  basePath: "/app",
+  experimental: {
+    serverComponentsExternalPackages: ['@tremor/react']
+  }
+};
+
+module.exports = nextConfig;
